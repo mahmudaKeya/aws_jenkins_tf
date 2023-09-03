487147b114f4f42cbc0ae3157d2dc3ad245d99db
mahmudaKeya <51240859+mahmudaKeya@users.noreply.github.com>
2023-09-03 17:19:25 +0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -18,34 +18,7 @@ resource "aws_subnet" "pubsub_1" {
   }
 }
 
-# Creating internet gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.my_vpc.id
 
-  tags = {
-    Name = "pubsub-1-igw"
-  }
-}
-
-# Creating route table
-resource "aws_route_table" "pubsub-1-rt" {
-  vpc_id = aws_vpc.my_vpc.id #required
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  tags = {
-    Name = "pubsub-1-rt"
-  }
-}
-
-# Subnet association to route table
-resource "aws_route_table_association" "pubsub_1_association" {
-  subnet_id      = aws_subnet.pubsub_1.id
-  route_table_id = aws_route_table.pubsub-1-rt.id
-}
 
 #security group
 resource "aws_security_group" "vpc-ssh" {
@@ -56,8 +29,8 @@ resource "aws_security_group" "vpc-ssh" {
   vpc_id = aws_vpc.my_vpc.id
   ingress {
     description = "Allow Port 22"
-    from_port   = 22
-    to_port     = 22
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -74,14 +47,6 @@ resource "aws_security_group" "vpc-ssh" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow Port 3000"
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -108,26 +73,3 @@ resource "aws_security_group" "vpc-ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-# Create Security Group - Web Traffic
-# resource "aws_security_group" "vpc-web" {
-#   name        = "vpc-web"
-#   description = "Dev VPC Web"
-
-#   vpc_id = aws_vpc.my_vpc.id
-#   ingress {
-#     description = "Allow Port 80"
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   egress {
-#     description = "Allow all IP and Ports outbound"
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
